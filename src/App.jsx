516fe8033b9e9f6705239265347968cabb217eea
JDRL555 <joshuleal3@gmail.com>
2023-10-09 13:10:53 -0400

    feat: initial react structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='text-3xl font-semibold'>Formula input</h1>
+    </div>
+  )
+}
---
+++ src/components/TagContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TagContainer() {
+  return (
+    <div>
+      <span>Google GSuite Expense</span>
+      <h1>1</h1>
+    </div>
+  )
+}
